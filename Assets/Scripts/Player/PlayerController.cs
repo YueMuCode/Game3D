e25f07d4f87e90fe08be9b90cfcfc8beb6cc2b8d
LinYueMu <1466923976@qq.com>
2023-03-25 16:47:51 +0800

    1、场景的设计，渲染管线的处理、灯光的渲染。 2、完成了人物的第三人称视角移动 3、人物的跑动、跳跃

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController character;    
+    public Transform cam;
+    public Transform groundCheck;
+    public LayerMask layerMask;
+    [Header("移动速度及镜头旋转的速度等参数")]
+    public float speed = 10f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    [Header("重力参数和地面的检测参数")]
+    public float gravity = -9.8f;
+    public Vector3 velocity = Vector3.zero;
+    public float checkRadius = .01f;
+    public float jumoHeight = 3f;
+    private bool isGround;
+    [Header("人物的动画变量")]
+    private Animator anim;
+    float animSpeed;
+    public bool isInputBlocked = true;
+    public bool isMove;
+
+    
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    { 
+        PlayerMove();
+        PlayerAnimation();
+    }
+    void PlayerMove()//这个函数完成了人物的跑路功能，以及将重力的效果添加给人物，使人物在移动的时候能够下落
+    {
+        velocity.y += gravity * Time.deltaTime;
+        character.Move(velocity * Time.deltaTime);
+        isGround = Physics.CheckSphere(groundCheck.position, checkRadius, layerMask);
+        float horizontal=0f;
+        float vertical=0f;
+        if (isGround && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+        if (isGround && Input.GetButtonDown("Jump"))
+        {
+            //PlayerAnimation()
+        }
+        if (isInputBlocked)
+        {
+             horizontal = Input.GetAxisRaw("Horizontal");
+             vertical = Input.GetAxisRaw("Vertical");
+        }
+       
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+         //   character.Move(moveDir.normalized * speed * Time.deltaTime);,因为下方的函数使用了movdToWards,就是朝中面向的方向平移，那么这里可用可不用
+
+            isMove = true;
+        }else
+        {
+            isMove = false;
+        }
+        
+
+    }
+    void PlayerAnimation()//目前这个函数触发跑路、跳跃动画，触发跳跃功能
+    {
+
+        // Vector3 horizontalVelocity = new Vector3(character.velocity.x, 0, character.velocity.z);
+        float deSpeed = 0f;
+        if (isMove)
+        {
+            deSpeed = speed;
+        }
+        animSpeed = Mathf.MoveTowards(animSpeed, deSpeed,Time.deltaTime*20);;
+        anim.SetFloat("Speed", animSpeed);
+        isGround = Physics.CheckSphere(groundCheck.position, checkRadius, layerMask);
+        if (isGround && Input.GetButtonDown("Jump")&&isInputBlocked)
+        {
+            velocity.y = Mathf.Sqrt(jumoHeight * -2 * gravity);
+
+            anim.SetTrigger("Jump");
+        }
+        if(isGround)
+        {
+
+            anim.SetBool("Ground", isGround);
+            
+        }
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("Land"))
+        {           
+            isInputBlocked = false;
+        }
+        else
+        {
+            isInputBlocked = true;
+        }
+      
+    }
+}
